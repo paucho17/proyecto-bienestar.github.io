38b3f7d78819d1198b9b142e36aa3cca525a3aad
paucho17 <pauchavarrom@gmail.com>
2026-08-20 13:46:45 -0500

    Fix2

--- bienestar_iub/settings.py
+++ bienestar_iub/settings.py
@@ -154,6 +154,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [ BASE_DIR / "static", ]
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Email
